287af00366f016fc8e044536f869638d83c52e25
Mena-Osorio-Christian <christianmena03@gmail.com>
2019-11-19 23:52:41 -0700

    Add project files.

---
+++ Data/LdsMovieAppContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace LdsMovieApp.Models
+{
+    public class LdsMovieAppContext : DbContext
+    {
+        public LdsMovieAppContext (DbContextOptions<LdsMovieAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<LdsMovieApp.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ Migrations/20191117065613_Rating.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LdsMovieApp.Migrations
+{
+    public partial class Rating : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Ratings",
+                table: "Movie");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Rating",
+                table: "Movie",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Movie");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Ratings",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
